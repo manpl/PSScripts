7c1cfff9ebbb67c657b1702055db736d897fe04e
manpl <nawrockim@op.pl>
2015-06-10 11:37:09 +0200

    Update Search-Items.psm1

--- Search-Items.psm1
+++ Search-Items.psm1
@@ -1,1 +1,23 @@
+Function Search-Items
+{
+	[CmdletBinding()]
+	Param(
+	  [Parameter(Mandatory=$True, Position=1)]
+	  [string]$searchPhrase,
+	  
+	  [Parameter(Mandatory=$False, Position=2)]
+	  [string]$filter
+	)
 
+	Write-Debug "$searchPhrase - $filter"
+	
+	if($filter){
+		$items = Get-ChildItem -R -Filter $filter 
+	}
+	else{
+		$items = Get-ChildItem -R 
+	}
+	
+	$result = $items | Select-String $searchPhrase 
+	$result | Write-Output
+}
